9badf848950c46f0b2e4fa2254e0944e6cced8ec
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-12-15 08:36:17 +0300

    layouts-created

---
+++ firebase-auth-v2/src/components/DefaultLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DefaultLayout = () => {
+  return (
+    <div>
+      defaultLaoyu
+    </div>
+  )
+}
+
+export default DefaultLayout
---
+++ firebase-auth-v2/src/components/GuestLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuestLayout = () => {
+  return (
+    <div>
+      GuestLayout
+    </div>
+  )
+}
+
+export default GuestLayout
